0c342251f1268297f7965868e220e618e592bb55
jack153566 <133614940+jack153566@users.noreply.github.com>
2023-06-12 00:11:42 -0600

    Finished SuitesPiles class
    Finished SuitesPiles class.

--- board/SuitePiles.java
+++ board/SuitePiles.java
@@ -1,5 +1,63 @@
 package board;
+import cards.Card;
+import cards.Suite;
 
 public class SuitePiles {
-
+	
+	//class variables
+	private Card[] heartPile;
+	private Card[] diamondPile;
+	private Card[] spadePile;
+	private Card[] clubPile;
+	
+	/**
+	 * Constructor
+	 */
+	SuitePiles()
+	{
+		
+		heartPile = new Card[13];
+		diamondPile = new Card[13];
+		spadePile = new Card[13];
+		clubPile = new Card[13];
+		
+	}
+	
+	/**
+	 * pile getter
+	 * 
+	 * @param suite
+	 * @return
+	 */
+	public Card[] getPile(Suite suite)
+	{
+		
+		switch (suite) 
+		{
+		
+			case CLUB:
+				
+				return clubPile;
+				
+			case SPADE:
+				
+				return spadePile;
+				
+			case DIAMOND:
+				
+				return diamondPile;
+				
+			case HEART:
+				
+				return heartPile;	
+				
+			default:
+				
+				return null;
+				
+		}
+		
+	}
+	
+	
 }
