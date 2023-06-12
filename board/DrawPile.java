7ce177a08d1ddbe7ef26142c6aba4c5f5f26013f
jack153566 <133614940+jack153566@users.noreply.github.com>
2023-06-12 00:21:11 -0600

    Finished DrawPile
    Finished DrawPile class

--- board/DrawPile.java
+++ board/DrawPile.java
@@ -1,5 +1,24 @@
 package board;
+import cards.Card;
+import java.util.List;
 
 public class DrawPile {
-
+	
+	//class variables
+	private List<Card> drawPile;
+	
+	//Constructor
+	DrawPile()
+	{
+		
+	}
+	
+	
+	public List<Card> getDrawPile()
+	{
+		
+		return this.drawPile;
+		
+	}
+	
 }
