d6dbb6b5a263b52fe4931f8ff25b63c6699e9716
jack153566 <133614940+jack153566@users.noreply.github.com>
2023-06-23 16:40:02 -0600

    Finished MoveCardsToOtherColumn method
    Finished MoveCardsToOtherColumn method.

--- gameplay/ColumnMoves.java
+++ gameplay/ColumnMoves.java
@@ -1,19 +1,31 @@
 package gameplay;
+import backend.BackendGameplay;
+import backend.ValidityChecks;
+import board.Column;
+import cards.*;
+
 
 public abstract class ColumnMoves {
 	
-	//TODO FINISH METHOD
-	public void MoveCardsToOtherColumn()
+	
+	public void MoveCardsToOtherColumn(Card targetCard, Card selectedCard, Column targetCol)
 	{
+		
+		if(ValidityChecks.CheckValidColumnMove(targetCard,selectedCard)==true)
+		{	
+			targetCol.GetFaceUpCards().add(targetCol.GetFaceUpCards().indexOf(targetCard), selectedCard);
+			
+		}
+		
 		
 	}
 	
 	//TODO FINISH METHOD
-	public void MoveCardTosuitePile()
+	public void MoveCardTosuitePile(Column col, Card card )
 	{
 		
 	}
 	
-	
+	 
 
 }
